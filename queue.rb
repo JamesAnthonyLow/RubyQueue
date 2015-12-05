9ee55a71abd4ca977c5192ef8fd9b1b76cc44c65
JamesAnthonyLow <jamesalowenthal@gmail.com>
2015-12-05 12:11:53 -0500

    remove successful driver code

--- queue.rb
+++ queue.rb
@@ -18,29 +18,3 @@ class Queue
     !@head
   end
 end
-
-q = Queue.new
-puts q.empty?
-q.enqueue "j"
-puts q.empty?
-puts q.peek
-q.enqueue "a"
-puts q.empty?
-puts q.peek
-q.enqueue "m"
-puts q.empty?
-puts q.peek
-q.enqueue "e"
-puts q.empty?
-puts q.peek
-q.enqueue "s"
-
-print q.dequeue
-print q.dequeue
-print q.dequeue
-print q.dequeue
-print q.dequeue
-puts
-p q.dequeue
-
-puts q.empty?
